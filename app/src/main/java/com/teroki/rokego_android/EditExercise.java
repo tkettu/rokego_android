7bb2afd13539be7344b34ac770fbcb8f1d5e28dc
tkettu <terojkettunen@gmail.com>
2017-11-06 16:34:40 +0200

    Adding edit exercise activity.

---
+++ app/src/main/java/com/teroki/rokego_android/EditExercise.java
@@ -0,0 +1,56 @@
+package com.teroki.rokego_android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.teroki.rokego_db.DBHelper;
+
+
+/**
+ * Created by Tero on 6.11.2017.
+ */
+
+public class EditExercise extends Activity {
+
+    TextView exercise;
+    String exerciseStr;
+    Button deleteBtn;
+    long id;
+    private String LOG_TAG = "EditExercise";
+    DBHelper db;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_exercise);
+
+        db = new DBHelper(this);
+        Intent intent = getIntent();
+        exercise = (TextView) findViewById(R.id.single_exercise);
+
+        id = intent.getLongExtra(Exercises.EXERCISE_MSG, -1);
+        if ( id == -1){
+            Log.e(LOG_TAG, "Not iterable item");
+            exerciseStr = "ERROR, WRONG EXERCISE";
+        }else{
+            exerciseStr = db.getExercise((int)id).toString();
+        }
+
+
+        exercise.setText(exerciseStr);
+
+        deleteBtn = (Button) findViewById(R.id.delete_btn);
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                db.deleteExercise(db.getExercise((int)id));
+            }
+        });
+    }
+}
